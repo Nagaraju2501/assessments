9920ad6be271906f8641741ab8afb92adc11a310
Nagaraju2501 <109521333+Nagaraju2501@users.noreply.github.com>
2022-08-23 12:52:46 +0530

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Emp ID] INT NULL PRIMARY KEY, 
+    [Emp Name] VARCHAR(50) NULL, 
+    [Emp Role] VARCHAR(50) NULL, 
+    [Emp Location] VARCHAR(50) NULL, 
+    [DOJ] DATE NULL
+)
